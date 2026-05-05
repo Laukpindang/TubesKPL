d3b3024a8620d77d0644719499a51e7f0bd8918b
Laukpindang <ferdiansyahbandung3@gmail.com>
2026-05-05 17:25:37 +0700

    [FIX]: barang

--- Tubes.Core/Barang.cs
+++ Tubes.Core/Barang.cs
@@ -5,12 +5,22 @@
         public int id { get; set; }
         public string nama { get; set; }
         public int harga { get; set; }
+        public int stok { get; set; }
         public Barang() { }
-        public Barang(int id, string nama, int harga) 
+        public Barang(int id, string nama, int harga, int stok) 
         {
             this.id = id;
             this.nama = nama;
             this.harga = harga;
+            this.stok = stok;
+        }
+        public void KurangiStok(int jumlah)
+        {
+            if (jumlah > stok)
+            {
+                throw new InvalidOperationException("Stok tidak cukup");
+            }
+            stok -= jumlah;
         }
     }
 }
