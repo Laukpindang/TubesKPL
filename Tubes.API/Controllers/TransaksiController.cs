0249ef8def41708581642eedbc2dec001c812249
Mri-cia <rawikara8c@gmail.com>
2026-05-07 14:47:39 +0700

    [FEAT]: API project

---
+++ Tubes.API/Controllers/TransaksiController.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tubes.API.Controllers
+{
+    public class TransaksiController : Controller
+    {
+        private readonly List<string> t;
+    }
+}
